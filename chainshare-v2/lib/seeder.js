8a019ffc633899d52e360b791667e48815576f02
Preet Taparia <preettaparia@gmail.com>
2025-09-17 15:47:00 +0530

    improve seeder

--- chainshare-v2/lib/seeder.js
+++ chainshare-v2/lib/seeder.js
@@ -135,3 +135,5 @@ function formatBytes(bytes, decimals = 2) {
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
